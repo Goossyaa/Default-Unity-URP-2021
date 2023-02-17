91701880b1326ce64f230bd8e9283fbc871dde33
Goossyaa <85863811+Goossyaa@users.noreply.github.com>
2023-02-18 02:55:23 +1000

    Initial commit

---
+++ Assets/Third-party/Plugins/Lux URP Essentials/Demos/Scripts/ExtendedFlycam.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+
+
+namespace LuxLWRPEssentials.Demo {
+ 
+	public class ExtendedFlycam : MonoBehaviour
+	{
+
+
+	// slightly changed....
+	 
+		/*
+		EXTENDED FLYCAM
+			Desi Quintans (CowfaceGames.com), 17 August 2012.
+			Based on FlyThrough.js by Slin (http://wiki.unity3d.com/index.php/FlyThrough), 17 May 2011.
+	 
+		LICENSE
+			Free as in speech, and free as in beer.
+	 
+		FEATURES
+			WASD/Arrows:    Movement
+			          Q:    Dropp
+			          E:    Climb
+	                      Shift:    Move faster
+	                    Control:    Move slower
+	                        End:    Toggle cursor locking to screen (you can also press Ctrl+P to toggle play mode on and off).
+		*/
+	 
+		public float cameraSensitivity = 90;
+		public float climbSpeed = 4;
+		public float normalMoveSpeed = 10;
+		public float slowMoveFactor = 0.25f;
+		public float fastMoveFactor = 3;
+	 
+		private float rotationX = 0.0f;
+		private float rotationY = 0.0f;
+
+		private bool isOrtho = false;
+		private Camera cam;
+
+		public bool isActive = true;
+	 
+		void Start () {
+			rotationX = transform.eulerAngles.y;
+			cam = GetComponent<Camera>();
+			if (cam != null) {
+				isOrtho = cam.orthographic;
+			}
+			
+		}
+
+	 	void Update ()
+		{
+
+			if (Keyboard.current.spaceKey.IsPressed()) {
+				isActive = !isActive;
+			}
+			if (!isActive) {
+				return;
+			}
+
+			// Cache deltaTime!
+			var deltaTime = Time.deltaTime;	
+			rotationX += Mouse.current.delta.x.ReadValue() * cameraSensitivity * deltaTime;
+			rotationY += Mouse.current.delta.y.ReadValue() * cameraSensitivity * deltaTime;
+			rotationY = Mathf.Clamp (rotationY, -90, 90);
+	 
+			var tempRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+			tempRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, tempRotation, deltaTime * 6.0f);
+	 
+		 	if (Keyboard.current.leftShiftKey.IsPressed() || Keyboard.current.rightShiftKey.IsPressed())
+		 	{
+				transform.position += transform.forward * (normalMoveSpeed * fastMoveFactor) * (Keyboard.current.wKey.ReadValue() - Keyboard.current.sKey.ReadValue()) * deltaTime;
+				transform.position += transform.right * (normalMoveSpeed * fastMoveFactor) * (Keyboard.current.dKey.ReadValue() - Keyboard.current.aKey.ReadValue())  * deltaTime;
+		 	}
+		 	else if (Keyboard.current.leftCtrlKey.IsPressed() || Keyboard.current.rightCtrlKey.IsPressed())
+		 	{
+				transform.position += transform.forward * (normalMoveSpeed * slowMoveFactor) * (Keyboard.current.wKey.ReadValue() - Keyboard.current.sKey.ReadValue()) * deltaTime;
+				transform.position += transform.right * (normalMoveSpeed * slowMoveFactor) * (Keyboard.current.dKey.ReadValue() - Keyboard.current.aKey.ReadValue()) * deltaTime;
+		 	}
+		 	else
+		 	{
+				if(isOrtho) {
+					cam.orthographicSize *= (1.0f - (Keyboard.current.wKey.ReadValue() - Keyboard.current.sKey.ReadValue()) * deltaTime);
+				}
+				else {
+					transform.position += transform.forward * normalMoveSpeed * (Keyboard.current.wKey.ReadValue() - Keyboard.current.sKey.ReadValue()) * deltaTime;
+				}
+				transform.position += transform.right * normalMoveSpeed * (Keyboard.current.dKey.ReadValue() - Keyboard.current.aKey.ReadValue()) * deltaTime;
+		 	}
+	 
+			if (Keyboard.current.qKey.IsPressed()) {transform.position -= transform.up * climbSpeed * deltaTime;}
+			if (Keyboard.current.eKey.IsPressed()) {transform.position += transform.up * climbSpeed * deltaTime;}
+		}
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,115 @@
+# Default-Unity-URP
+Unity 2020.3.43f1
+
+# [0.1.10] — 2023-01-06
+
+### Добавлено
+1. DOTweenPro
+2. Horizon Based Ambient Occlusion
+
+
+# [0.1.9] — 2023-01-05
+
+### Добавлено
+1. Feel
+2. Bolt
+3. FMOD
+4. I2 Localization
+5. Lux URP Essentials
+
+### Исправлено
+Изменил настройки Input System на Both
+
+### Удалено 
+Папка Assets/Third-party/Editor/Grabbit/Demo
+
+
+
+
+# [0.1.8] — 2023-01-05
+
+Добавил ShaderControl
+
+
+# [0.1.7] — 2023-01-05
+
+Перенес Grabbit в Assets/Third-party/Editor
+
+
+
+# [0.1.6] — 2023-01-05
+
+Добавил Beautify 3
+
+
+# [0.1.5] — 2023-01-05
+
+Добавил Grabbit
+
+
+# [0.1.4] — 2023-01-05
+
+### Добавлено
+1. 2D Animations
+2. Cinemachine
+3. Input System (New)
+4. Post Processing
+5. ProBuilder
+6. Unity Recorder
+7. Visual Effect Graph
+
+
+
+# [0.1.3] — 2023-01-05
+
+Добавил TextMesh Pro
+
+
+# [0.1.2] — 2023-01-05
+
+Добавил BuildReport. И настроил папки по умолчанию
+
+
+# [0.1.1] — 2023-01-05
+
+Добавил ConsolePro
+Удалил сгенерившуюся папку Assets/Third-party/Plugins
+
+
+# [0.1.0] — 2023-01-05
+
+Переименовал папку **Assets/Third-party/Plugins** в **Editor**. Теперь ассеты из этой папки не будут включатся в билд. Что избавило от компиляционных ошибок из-за **HierarhyPro**
+
+
+# [0.0.6] — 2023-01-05
+
+Добавил **HierarchyPro** и настроил
+
+
+# [0.0.5] — 2023-01-05
+
+### Добавлено
+1. Hierarhy 2
+2. RainbowFolders
+3. SuperPivot
+
+
+# [0.0.4] — 2023-01-05
+
+Добавил структуру папок по **Ramen Unity Style Guide** из https://github.com/Goossyaa/Unity-file-structure-exapmle
+
+
+# [0.0.3] — 2023-01-05
+
+Удалил стандартные ассеты. Кроме папок с настройками, пресетами и скриптами
+
+
+# [0.0.2] — 2023-01-05
+
+1. Создан проект Юнити по шаблону **3D Sample Scene (URP)**
+2. Добавлен .gitignore
+
+
+# [0.0.1] — 2023-01-05
+
+Создан репозиторий
